c6213ca9bdaa4e0d35f68b2c1f69ce2531842bc4
Dragon-Seeker <timekeeperguild@gmail.com>
2025-03-20 22:02:48 -0500

    Update mixin injection to prevent trinket items from getting default renderer

--- neoforge/src/main/java/top/theillusivec4/curios/mixin/core/accessories/client/AccessoriesRendererRegistryMixin.java
+++ neoforge/src/main/java/top/theillusivec4/curios/mixin/core/accessories/client/AccessoriesRendererRegistryMixin.java
@@ -1,9 +1,9 @@
 package top.theillusivec4.curios.mixin.core.accessories.client;
 
+import com.google.common.collect.BiMap;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.wispforest.accessories.api.client.AccessoriesRendererRegistry;
-import io.wispforest.accessories.api.client.AccessoryRenderer;
 import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,20 +11,18 @@ import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.common.capability.ItemizedCurioCapability;
 import top.theillusivec4.curios.compat.AccessoryFromCurio;
 
-import java.util.Map;
-
 @Mixin(value = AccessoriesRendererRegistry.class, remap = false)
 public abstract class AccessoriesRendererRegistryMixin {
-    @WrapOperation(method = "Lio/wispforest/accessories/api/client/AccessoriesRendererRegistry;getRender(Lnet/minecraft/world/item/Item;)Lio/wispforest/accessories/api/client/AccessoryRenderer;", at = @At(value = "INVOKE", target = "Ljava/util/Map;getOrDefault(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"), remap = false)
-    private static Object alterDefaultBehavior(Map<Item, AccessoryRenderer> CACHED_RENDERERS, Object item, Object defaultRenderer, Operation<Object> operation) {
-        var stack = ((Item) (item)).getDefaultInstance();
+    @WrapOperation(method = "getRenderer(Lnet/minecraft/world/item/Item;)Lio/wispforest/accessories/api/client/AccessoryRenderer;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/BiMap;containsKey(Ljava/lang/Object;)Z"), remap = false)
+    private static boolean alterDefaultBehavior(BiMap map, Object item, Operation<Boolean> operation) {
+        var stack = ((Item) item).getDefaultInstance();
 
         var iCurioItem = CuriosApi.getCurio(stack).orElse(null);
 
         if(!(iCurioItem instanceof ItemizedCurioCapability capability && capability.curioItem instanceof AccessoryFromCurio)) {
-            defaultRenderer = null;
+            return true;
         }
 
-        return operation.call(CACHED_RENDERERS, item, defaultRenderer);
+        return operation.call(map, item);
     }
 }
