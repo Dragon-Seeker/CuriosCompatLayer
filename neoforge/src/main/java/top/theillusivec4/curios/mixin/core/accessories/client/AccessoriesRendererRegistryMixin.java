05881e077ce5877d90187ab37a02958277f8d8a0
Dragon-Seeker <timekeeperguild@gmail.com>
2025-03-21 00:29:36 -0500

    Fix incorrect arg for getting possible curios in renderer get mixin

--- neoforge/src/main/java/top/theillusivec4/curios/mixin/core/accessories/client/AccessoriesRendererRegistryMixin.java
+++ neoforge/src/main/java/top/theillusivec4/curios/mixin/core/accessories/client/AccessoriesRendererRegistryMixin.java
@@ -3,6 +3,7 @@ package top.theillusivec4.curios.mixin.core.accessories.client;
 import com.google.common.collect.BiMap;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import io.wispforest.accessories.api.client.AccessoriesRendererRegistry;
 import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,8 +15,8 @@ import top.theillusivec4.curios.compat.AccessoryFromCurio;
 @Mixin(value = AccessoriesRendererRegistry.class, remap = false)
 public abstract class AccessoriesRendererRegistryMixin {
     @WrapOperation(method = "getRenderer(Lnet/minecraft/world/item/Item;)Lio/wispforest/accessories/api/client/AccessoryRenderer;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/BiMap;containsKey(Ljava/lang/Object;)Z"), remap = false)
-    private static boolean alterDefaultBehavior(BiMap map, Object item, Operation<Boolean> operation) {
-        var stack = ((Item) item).getDefaultInstance();
+    private static boolean alterDefaultBehavior(BiMap map, Object object, Operation<Boolean> operation, @Local(argsOnly = true) Item item) {
+        var stack = item.getDefaultInstance();
 
         var iCurioItem = CuriosApi.getCurio(stack).orElse(null);
 
@@ -23,6 +24,6 @@ public abstract class AccessoriesRendererRegistryMixin {
             return true;
         }
 
-        return operation.call(map, item);
+        return operation.call(map, object);
     }
 }
